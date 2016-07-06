8267133bcfd73de6a7bc94bc28cbe5714d6c09a3
Lincoln Fernandes Coelho <lincolnfcoelho@yahoo.com.br>
2016-07-05 21:17:03 -0300

    Commit EstudoSpring

---
+++ src/main/java/br/com/estudo/spring/microservices/Calculadora.java
@@ -0,0 +1,27 @@
+package br.com.estudo.spring.microservices;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Calculadora {
+	
+	private final static String versao = "2.0"; 
+	
+	@RequestMapping("/")
+	public String obterVersao() {
+		return versao;
+	}
+	
+	public double somar(double numero1, double numero2) {
+		return numero1 + numero2;
+	}
+	
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(Calculadora.class, args);
+    }
+
+}
